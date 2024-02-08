acfa73a5bae161c1cad927862c53dba2e2a6f5b3
inaciocr04 <inaciocr04@gmail.com>
2024-02-08 11:08:18 +0100

    site V7

--- js/main.js
+++ js/main.js
@@ -1,33 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let sliderContainer = document.querySelector('.slider-container');
-    let slider = document.querySelector('.slider');
+document.addEventListener("DOMContentLoaded", function () {
+  let sliderContainer = document.querySelector(".slider-container");
+  let slider = document.querySelector(".slider");
 
-    let initialX = null;
-    let initialScrollY = window.scrollY + 500;
-    let middleY = window.innerHeight / 2;
+  let initialX = null;
+  let initialScrollY = window.scrollY + 500;
+  let middleY = window.innerHeight / 2;
 
-    window.addEventListener('scroll', function () {
-        if (initialX === null) {
-            initialX = slider.getBoundingClientRect().left + window.scrollX;
-        }
+  window.addEventListener("scroll", function () {
+    if (initialX === null) {
+      initialX = slider.getBoundingClientRect().left + window.scrollX;
+    }
 
-        let scrollY = window.scrollY + 500;
+    let scrollY = window.scrollY + 500;
 
-        let offsetX = (scrollY - initialScrollY) * 1;
+    let offsetX = (scrollY - initialScrollY) * 1;
 
-        slider.style.transform = `translateX(${initialX - offsetX}px)`;
-    });
+    slider.style.transform = `translateX(${initialX - offsetX}px)`;
+  });
 });
 
-function togglemenu(){
-    const navbar = document.querySelector('.navbar-mobile');
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click', () => {
- navbar.classList.toggle('show-nav');
-    })
+function togglemenu() {
+  const navbar = document.querySelector(".navbar-mobile");
+  const burger = document.querySelector(".burger");
+  burger.addEventListener("click", () => {
+    navbar.classList.toggle("show-nav");
+  });
 }
 togglemenu();
 
+// Chart.register(ChartDeferred);
 
 const ctx1 = document.getElementById("chart2");
 
@@ -37,7 +38,11 @@ const chart2 = new Chart(ctx1, {
     datasets: [
       {
         data: myData1["chiffres"],
-        backgroundColor: ["rgb(92,203,123,0.5)", "rgb(191,239,211,0.5)", "rgb(83,156,103,0.5)  "],
+        backgroundColor: [
+          "rgb(92,203,123,0.5)",
+          "rgb(191,239,211,0.5)",
+          "rgb(83,156,103,0.5)  ",
+        ],
         borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
       },
@@ -55,55 +60,59 @@ const chart2 = new Chart(ctx1, {
         display: true,
         text: "Impact de l'installation de dispositifs hydro-économes",
         font: {
-          size: 18
-        }
+          size: 18,
+        },
       },
       legend: {
-        display: true,
-        position: 'bottom',
+        display: false,
+        position: "bottom",
       },
       tooltip: {
         callbacks: {
-          label: function(context) {
-            var label = context.label || '';
+          label: function (context) {
+            var label = context.label || "";
             if (label) {
-              label += ': ';
+              label += ": ";
             }
-            label += context.formattedValue + ' kWh';
+            label += context.formattedValue + " kWh";
             return label;
-          }
-        }
-      }
+          },
+        },
+      },
     },
     layout: {
       padding: {
-        top: 20
-      }
+        top: 20,
+      },
     },
     elements: {
       line: {
         borderWidth: 2,
-        borderColor: 'rgba(0, 0, 0, 0.5)',
+        borderColor: "rgba(0, 0, 0, 0.5)",
       },
       arc: {
-        borderWidth: 0
-      }
+        borderWidth: 0,
+      },
     },
     scales: {
       r: {
+        pointLabels: {
+          display: true,
+          centerPointLabels: true,
+          font: {
+            size: 18,
+          },
+        },
         ticks: {
           stepSize: 50,
-          callback: function(value) {
-            return value + ' kWh';
-          }
-        }
-      }
+          callback: function (value) {
+            return value + " kWh";
+          },
+        },
+      },
     },
   },
 });
-
-
-
 
 const ctx2 = document.getElementById("chart3");
 
@@ -116,7 +125,6 @@ const chart3 = new Chart(ctx2, {
         backgroundColor: "rgba(75, 192, 192, 0.5)",
         borderColor: "rgba(75, 192, 192, 1)",
         borderWidth: 1,
-        
       },
     ],
     labels: myData2["annees"],
@@ -139,16 +147,16 @@ const chart3 = new Chart(ctx2, {
   },
   plugins: {
     title: {
-        display: true,
-        text: "Graphique",
-        font: {
-            size: 18
-        }
+      display: true,
+      text: "Graphique",
+      font: {
+        size: 18,
+      },
     },
     legend: {
-        display: true,
-        position: 'bottom', // 'top', 'bottom', 'left', 'right'
-        // ...
+      display: true,
+      position: "bottom", // 'top', 'bottom', 'left', 'right'
+      // ...
     },
   },
 });
@@ -180,7 +188,7 @@ const chart = new Chart(ctx, {
       {
         data: data[3].chiffres,
         label: data[3].catégorie,
-      }
+      },
     ],
   },
   options: {
@@ -190,7 +198,7 @@ const chart = new Chart(ctx, {
       },
       delay: (context) => {
         let delay = 0;
-        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+        if (context.type === "data" && context.mode === "default" && !delayed) {
           delay = context.dataIndex * 200 + context.datasetIndex * 2000;
         }
         return delay;
@@ -202,16 +210,15 @@ const chart = new Chart(ctx, {
     scales: {
       x: {
         grid: {
-          display: false
-        }
+          display: false,
+        },
       },
       y: {
         beginAtZero: true,
         max: 50,
-      }
+      },
     },
     plugins: {
-
       autocolors: {
         offset: 39,
       },
@@ -220,80 +227,154 @@ const chart = new Chart(ctx, {
         text: "Energie de chauffage par classe des logements en %",
         font: {
           size: 18,
-        }
-      }
-    }
-  }
-
+        },
+      },
+      legend: {
+        display: true,
+        position: "bottom",
+        labels: {
+          font: {
+            size: 8,
+          },
+        },
+      },
+    },
+  },
 });
 
 const ctx3 = document.getElementById("chart1");
 
 const chart1 = new Chart(ctx3, {
-    type: "doughnut",
-    data: {
-        datasets: [{
-            data: data2.proportions
-        }],
+  type: "doughnut",
+  data: {
+    datasets: [
+      {
+        data: data2.proportions,
+      },
+    ],
 
-        labels: data2.catégories
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: false,        
-        /* datasets:{
+    labels: data2.catégories,
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    /* datasets:{
             pie: {
                 radius: 180,
                 spacing: 1
             }
         }, */
-        
-        plugins: {
-            title: {
-                display: true,
-                text: "Graphique",
-                font: {
-                    size: 18
-                }
-            },
-            legend: {
-                display: true,
-                position: 'bottom', // 'top', 'bottom', 'left', 'right'
-                // ...
-            },
+
+    plugins: {
+      title: {
+        display: true,
+        text: "Graphique",
+        font: {
+          size: 18,
         },
-        animation: {
-          animateRotate: true,
-          animateScale: true,
-          duration: 2000,
-          easing: 'easeInOutQuart',
-      }
-    }
+      },
+      legend: {
+        display: true,
+        position: "bottom", // 'top', 'bottom', 'left', 'right'
+        labels: {
+          font: {
+            size: 8,
+          },
+        },
+      },
+    },
+    animation: {
+      animateRotate: true,
+      animateScale: true,
+      duration: 2000,
+      easing: "easeInOutQuart",
+    },
+  },
 });
 
-// Récupérer la position verticale de l'élément contenant le graphique
-const chartContainer = document.getElementById("chartContainer");
-const chartContainerPosition = chartContainer.offsetTop;
+// // Récupérer la position verticale de l'élément contenant le graphique
+// const chartContainer = document.getElementById("chartContainer");
+// const chartContainerPosition = chartContainer.offsetTop;
 
-// Fonction pour déclencher l'animation du graphique
-function triggerChartAnimation() {
-    chart2.update(); // Mettre à jour le graphique pour déclencher l'animation
-    chart3.update(); // Mettre à jour le graphique pour déclencher l'animation
-    chart.update(); // Mettre à jour le graphique pour déclencher l'animation
-    chart1.update(); // Mettre à jour le graphique pour déclencher l'animation
-    window.removeEventListener('scroll', scrollHandler); // Retirer l'écouteur d'événement après avoir déclenché l'animation
-}
+// // Fonction pour déclencher l'animation du graphique
+// function triggerChartAnimation() {
+//   chart2.update(); // Mettre à jour le graphique pour déclencher l'animation
+//   chart3.update(); // Mettre à jour le graphique pour déclencher l'animation
+//   chart.update(); // Mettre à jour le graphique pour déclencher l'animation
+//   chart1.update(); // Mettre à jour le graphique pour déclencher l'animation
+//   window.removeEventListener("scroll", scrollHandler); // Retirer l'écouteur d'événement après avoir déclenché l'animation
+// }
 
-// Fonction pour gérer l'événement de défilement de la fenêtre
-function scrollHandler() {
-    // Récupérer la position actuelle de défilement
-    const scrollPosition = window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
+// // Fonction pour gérer l'événement de défilement de la fenêtre
+// function scrollHandler() {
+//   // Récupérer la position actuelle de défilement
+//   const scrollPosition =
+//     window.scrollY || window.pageYOffset || document.documentElement.scrollTop;
 
-    // Vérifier si la position de défilement atteint ou dépasse la position de l'élément contenant le graphique
-    if (scrollPosition >= chartContainerPosition) {
-        triggerChartAnimation(); // Déclencher l'animation du graphique
-    }
-}
+//   // Vérifier si la position de défilement atteint ou dépasse la position de l'élément contenant le graphique
+//   if (scrollPosition >= chartContainerPosition) {
+//     triggerChartAnimation(); // Déclencher l'animation du graphique
+//   }
+// }
 
-// Ajouter un écouteur d'événement pour le défilement de la fenêtre
-window.addEventListener('scroll', scrollHandler);
+// // Ajouter un écouteur d'événement pour le défilement de la fenêtre
+// window.addEventListener("scroll", scrollHandler);
+
+document.addEventListener("DOMContentLoaded", function () {
+  var ctx4 = document.getElementById("myChart").getContext("2d");
+  var years = ["2022", "2030", "À terme"];
+  var percentages = [20.7, 33, 100];
+
+  var myChart = new Chart(ctx4, {
+    type: "bar",
+    data: {
+      labels: years,
+      datasets: [
+        {
+          label: "Production d'Énergie Renouvelable en France (%)",
+          data: percentages,
+          backgroundColor: ["#6ADC8B", "#5AD87E", "#41D36A"],
+          borderColor: ["#E4E4E4", "#E4E4E4", "#E4E4E4"],
+          borderWidth: 2,
+          borderRadius: {
+            topLeft: 10,
+            topRight: 10,
+          },
+          hoverBackgroundColor: ["#49D471", "#39D064", "#E34F26"],
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        y: {
+          display: false,
+        },
+        x: {
+          display: false,
+        },
+      },
+      elements: {
+        line: {
+          borderWidth: 0,
+        },
+        point: {
+          radius: 0,
+        },
+      },
+      maintainAspectRatio: false,
+      plugins: {
+        deferred: {
+          xOffset: 150, // defer until 150px of the canvas width are inside the viewport
+          yOffset: "90%", // defer until 50% of the canvas height are inside the viewport
+          delay: 200, // delay of 500 ms after the canvas is considered inside the viewport
+        },
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+});
